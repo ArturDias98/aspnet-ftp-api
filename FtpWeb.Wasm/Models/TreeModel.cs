a94dd4f1bc53066254bd0d8ab66fc319447ddead
Artur Dias <arturdrm@hotmail.com>
2023-09-24 10:57:08 -0300

    Tree view

---
+++ FtpWeb.Wasm/Models/TreeModel.cs
@@ -0,0 +1,14 @@
+﻿using FtpWeb.Shared.Models;
+using MudBlazor;
+
+namespace FtpWeb.Wasm.Models;
+
+public record TreeModel(string Name, string FullName, bool IsFolder)
+    : FtpItemModel(Name, FullName, IsFolder)
+{
+    public HashSet<TreeModel> Items { get; set; } = new();
+    public string Icon => IsFolder
+        ? Icons.Material.Filled.Folder
+        : Icons.Custom.FileFormats.FileDocument;
+    public bool CanExpand => Items.Count > 0;
+}
---
+++ FtpWeb.Wasm/Pages/Index.razor.cs
@@ -0,0 +1,18 @@
+﻿using FtpWeb.Wasm.Models;
+
+namespace FtpWeb.Wasm.Pages;
+
+public partial class Index
+{
+    private HashSet<TreeModel> _treeItems = new();
+
+    private async Task<HashSet<TreeModel>> LoadItems(TreeModel item)
+    {
+        return item.Items;
+    }
+
+    protected override Task OnInitializedAsync()
+    {
+        return base.OnInitializedAsync();
+    }
+}
